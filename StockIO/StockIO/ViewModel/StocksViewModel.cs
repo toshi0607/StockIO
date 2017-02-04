7e54ad4bf793de095f0f976a7aa54636a61fec28
toshi0607 <s.toshi0607@gmail.com>
2017-02-04 17:11:24 +0900

    Stock用のViewModelの追加

---
+++ StockIO/StockIO/ViewModel/StocksViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace StockIO.ViewModel
+{
+    class StocksViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        bool busy;
+        public bool IsBusy
+        {
+            get { return busy; }
+            set
+            {
+                busy = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
