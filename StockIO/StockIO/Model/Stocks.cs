6f11ebaf9e3962b575470b1b320efcc92163c412
toshi0607 <s.toshi0607@gmail.com>
2017-02-04 15:52:49 +0900

    Stockモデル追加

---
+++ StockIO/StockIO/Model/Stocks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockIO.Model
+{
+    class Stocks
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public int Amount { get; set; }
+        public int ThresholdAmount { get; set; }
+    }
+}
