c7f608380e3dd1c430149e04365e1fa4a88800b7
toshi0607 <s.toshi0607@gmail.com>
2017-02-11 22:44:28 +0900

    Stockの編集をプロパティで保持する

--- StockIO/StockIO/View/StockEditPage.xaml.cs
+++ StockIO/StockIO/View/StockEditPage.xaml.cs
@@ -20,6 +20,35 @@ namespace StockIO.View
             this.stock = item;
 
             BindingContext = this.stock;
+
+            Name.Completed += Name_Completed;
+            Amount.TextChanged += Amount_TextChanged;
+            ThresholdAmount.TextChanged += ThresholdAmount_TextChanged;
+
+        }
+
+        private void Name_Completed(object sender, EventArgs e)
+        {
+            stock.Name = ((Entry)sender).Text;
+            return;
+        }
+
+        private void Amount_TextChanged(object sender, EventArgs e)
+        {
+            int result;
+            bool isValid = int.TryParse(((Entry)sender).Text, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+            stock.Amount = isValid ? result : stock.Amount;
+            return;
+        }
+
+        private void ThresholdAmount_TextChanged(object sender, EventArgs e)
+        {
+            int result;
+            bool isValid = int.TryParse(((Entry)sender).Text, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+            stock.ThresholdAmount = isValid ? result : stock.ThresholdAmount;
+            return;
         }
     }
 }
