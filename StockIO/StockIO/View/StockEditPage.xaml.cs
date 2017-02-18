8c1e0ccbdcc113406f74dddfc9da8011ce1fa039
toshi0607 <s.toshi0607@gmail.com>
2017-02-18 22:46:41 +0900

    EditPageをViewModel仕様に

--- StockIO/StockIO/View/StockEditPage.xaml.cs
+++ StockIO/StockIO/View/StockEditPage.xaml.cs
@@ -1,133 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
+﻿using Xamarin.Forms;
 
-using Xamarin.Forms;
-
+using StockIO.ViewModel;
 using StockIO.Model;
-using StockIO.Services;
 
 namespace StockIO.View
 {
     public partial class StockEditPage : ContentPage
     {
-        Stock preStock;
-        Stock stock;
+        StockEditViewModel vm;
+
         public StockEditPage(Stock item)
         {
             InitializeComponent();
 
-            this.preStock = item;
-            this.stock = item;
+            vm = new StockEditViewModel(item);
 
-            BindingContext = this.stock;
+            vm.OnUpdate += async () =>
+            {
+                await Navigation.PushAsync(new StocksPage());
+            };
+            vm.OnDelete += async () =>
+            {
+                await Navigation.PushAsync(new StocksPage());
+            };
 
-            Name.Completed += Name_Completed;
-            Amount.TextChanged += Amount_TextChanged;
-            ThresholdAmount.TextChanged += ThresholdAmount_TextChanged;
-            SaveButton.Clicked += OnSaveButtonClicked;
-            DeleteButton.Clicked += OnDeleteButtonClicked;
+            BindingContext = vm;
 
             if (item.ID == null)
                 DeleteButton.IsEnabled = false;
         }
 
-        private void Name_Completed(object sender, EventArgs e)
-        {
-            stock.Name = ((Entry)sender).Text;
-            return;
-        }
-
-        private void Amount_TextChanged(object sender, EventArgs e)
-        {
-            int result;
-            bool isValid = int.TryParse(((Entry)sender).Text, out result);
-            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
-            stock.Amount = isValid ? result : stock.Amount;
-            return;
-        }
-
-        private void ThresholdAmount_TextChanged(object sender, EventArgs e)
-        {
-            int result;
-            bool isValid = int.TryParse(((Entry)sender).Text, out result);
-            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
-            stock.ThresholdAmount = isValid ? result : stock.ThresholdAmount;
-            return;
-        }
-
-        private async void OnDeleteButtonClicked(object sender, EventArgs e)
-        {
-            await DeleteStock();
-        }
-
-        async Task DeleteStock()
-        {
-            bool result = await Application.Current.MainPage.DisplayAlert("", "本当に削除しますか？", "はい", "いいえ");
-            if (!result)
-                return;
-
-            Exception error = null;
-            try
-            {
-                var service = DependencyService.Get<AzureService>();
-                await service.DeleteStock(this.stock);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Error: " + ex);
-                error = ex;
-            }
-
-            if (error != null)
-            {
-                await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
-            }
-            else
-            {
-                await Application.Current.MainPage.DisplayAlert("", "削除しました。", "OK");
-            }
-
-            await Navigation.PushAsync(new StocksPage());
-        }
-
-        private async void OnSaveButtonClicked(object sender, EventArgs e)
-        {
-            //if (this.preStock != this.stock)
-            //{
-                await SaveStock();
-            //}
-        }
-
-        async Task SaveStock()
-        {
-            Exception error = null;
-            try
-            {
-                var service = DependencyService.Get<AzureService>();
-                await service.SaveStock(this.stock);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Error: " + ex);
-                error = ex;
-            }
-
-            var result = false;
-            if (error != null)
-            {
-                await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
-            }
-            else
-            {
-                result = await Application.Current.MainPage.DisplayAlert("", "保存しました。一覧に戻りますか？", "はい", "いいえ");
-            }
-                
-            if (result)
-                await Navigation.PushAsync(new StocksPage());
-        }
     }
 }
---
+++ StockIO/StockIO/ViewModel/StockEditViewModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+
+using StockIO.Services;
+using StockIO.Model;
+
+namespace StockIO.ViewModel
+{
+    class StockEditViewModel : INotifyPropertyChanged
+    {
+        public Stock Stock { set; get; }
+        public Command SaveStockCommand { get; set; }
+        public Command DeleteStockCommand { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Action OnUpdate;
+        public Action OnDelete;
+
+        public StockEditViewModel(Stock stock)
+        {
+            Stock = stock;
+            SaveStockCommand = new Command(
+                async () => await SaveStock(),
+                () => !IsBusy);
+            DeleteStockCommand = new Command(
+                async () => await DeleteStock(),
+                () => !IsBusy);
+        }
+
+        void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        bool busy;
+        public bool IsBusy
+        {
+            get { return busy; }
+            set
+            {
+                busy = value;
+                OnPropertyChanged();
+                //Update the can execute
+                SaveStockCommand.ChangeCanExecute();
+                DeleteStockCommand.ChangeCanExecute();
+            }
+        }
+
+        async Task SaveStock()
+        {
+            if (IsBusy)
+                return;
+
+            Exception error = null;
+            try
+            {
+                IsBusy = true;
+
+                var service = DependencyService.Get<AzureService>();
+                await service.SaveStock(Stock);
+
+                var result = false;
+                if (error != null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
+                }
+                else
+                {
+                    result = await Application.Current.MainPage.DisplayAlert("", "保存しました。一覧に戻りますか？", "はい", "いいえ");
+                }
+
+                if (result)
+                    OnUpdate?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex);
+                error = ex;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        async Task DeleteStock()
+        {
+            if (IsBusy)
+                return;
+
+            bool result = await Application.Current.MainPage.DisplayAlert("", "本当に削除しますか？", "はい", "いいえ");
+            if (!result)
+                return;
+
+            Exception error = null;
+            try
+            {
+                IsBusy = true;
+
+                var service = DependencyService.Get<AzureService>();
+                await service.DeleteStock(Stock);
+
+                if (error != null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("", "削除しました。", "OK");
+                }
+
+                OnDelete?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex);
+                error = ex;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
