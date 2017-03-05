4c98403dc31b6b68bf1bede186f25c079145c0d0
toshi0607 <s.toshi0607@gmail.com>
2017-03-05 19:32:01 +0900

    OnPropertyChangedをメソッドに変更

--- StockIO/StockIO/ViewModel/BindableBase.cs
+++ StockIO/StockIO/ViewModel/BindableBase.cs
@@ -8,19 +8,21 @@ using System.Runtime.CompilerServices;
 
 namespace StockIO.ViewModel
 {
-    class BindableBase : INotifyPropertyChanged
+    public class BindableBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        void OnPropertyChanged([CallerMemberName] string name = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
         {
             if (object.Equals(field, value)) { return false; }
 
             field = value;
-            this.OnPropertyChanged(propertyName);
+            OnPropertyChanged(propertyName);
             return true;
         }
     }
