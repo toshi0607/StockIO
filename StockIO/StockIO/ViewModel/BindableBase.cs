d46ed2e711ad8bc2a60096befe8b167b7a6b2da8
toshi0607 <s.toshi0607@gmail.com>
2017-03-05 19:25:32 +0900

    BindableBase追加

---
+++ StockIO/StockIO/ViewModel/BindableBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StockIO.ViewModel
+{
+    class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(field, value)) { return false; }
+
+            field = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
